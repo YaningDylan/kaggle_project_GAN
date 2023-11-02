f1fdfe7ecc8bb4add493cd3477017f1e56b74d20
YaningDylan <130632600+YaningDylan@users.noreply.github.com>
2023-11-01 19:48:13 -0500

    convert ipynb to py

---
+++ Jupyter lab tester.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+print("hello world")
+
+print("Hi!!")
+
+
